1b8611cef63e922d76570ebb9ff34c40cd10cd75
Jack Thao <jackthao24@gmail.com>
2023-02-03 14:53:12 +0000

    Added For Loops and an Array of Strings

--- hello.sh
+++ hello.sh
@@ -6,3 +6,16 @@ c=$((a + b))
 
 echo "Bash says: Hello, World!"
 echo "$a + $b = $c"
+
+declare -a arr=("User1" "User2" "User3")
+
+echo "An Array of Strings are:"
+
+for i in "${arr[@]}"
+
+do
+
+echo "$i"
+
+done
+
